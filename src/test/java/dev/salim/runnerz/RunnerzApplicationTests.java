3adc1a7685ccedbc66c782a22dce962c4ec0b4ab
Salim <it.patteblanche@gmail.com>
2024-12-10 20:17:45 +0100

    Init project

---
+++ src/test/java/dev/salim/runnerz/RunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.salim.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
